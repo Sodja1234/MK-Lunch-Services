29eacbaaae71548698fa83faf613bba4573b7e7d
Sodja1234 <franklinkabisayi@gmail.com>
2025-03-25 18:40:34 +0100

    commit 2 : footer and section-service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SectionServiceComponent } from './section-service/section-service.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,
+    SectionServiceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/section-service/section-service.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-service',
+  imports: [],
+  templateUrl: './section-service.component.html',
+  styleUrl: './section-service.component.css'
+})
+export class SectionServiceComponent {
+
+}
